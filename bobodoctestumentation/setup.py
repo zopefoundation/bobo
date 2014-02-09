8f6fdd9b22b492d0c6dd9675a84e51f1fef33d93
Jim Fulton <jim@zope.com>
2014-02-09 12:53:51 -0500

    prep for release

--- bobodoctestumentation/setup.py
+++ bobodoctestumentation/setup.py
@@ -12,7 +12,7 @@
 #
 ##############################################################################
 name = 'bobodoctestumentation'
-version = '0.0.0'
+version = '2.0.0'
 
 from setuptools import setup
 
